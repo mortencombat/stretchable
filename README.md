d2aed640cb97040791b42113ff8a803843acaf81
dkkenv <kenv@sweco.dk>
2023-07-27 12:47:26 +0200

    Test build matrix

---
+++ README.md
@@ -0,0 +1,28 @@
+# Introduction
+
+This package enables target-agnostic layout operations using the CSS flexbox algorithm.
+
+It implements Python bindings for [Stretch](https://vislyhq.github.io/stretch/), an implementation of Flexbox written in [Rust](https://www.rust-lang.org/).
+
+The project is based on the translation of the bindings from Swift to Python, from [stretched](https://github.com/nmichlo/stretched)
+
+# Getting Started
+
+Helpful resources to getting started with CSS Flexbox include:
+
+- [Flexbox Froggy](https://flexboxfroggy.com/)
+- [Yoga Playground](https://yogalayout.com/playground)
+
+# Build and Test
+
+TODO: Explain how to build and test the package/code.
+
+To build the package distribution, use `python -m build`.
+
+To upload the package distribution, use `twine upload dist/* --repository stretchable`
+
+NOTE: All of this will be setup in GitHub Actions when the project is added to Github, build, test and uploading of package will be done automatically.
+
+# Contribute
+
+TODO: Explain how other users and developers can contribute.
---
+++ src/stretchable/stretch.py
@@ -0,0 +1,31 @@
+from . import _bindings
+
+# ========================================================================= #
+# STRETCH                                                                   #
+# ========================================================================= #
+
+
+class Stretch:
+    _PRIVATE_PTR: int = None
+
+    def __init__(self):
+        raise Exception(
+            "You should not be accessing or attempting to create an instance of this class."
+        )
+
+    @staticmethod
+    def get_ptr():
+        if Stretch._PRIVATE_PTR is None:
+            Stretch._PRIVATE_PTR = _bindings.stretch_init()
+        return Stretch._PRIVATE_PTR
+
+    @staticmethod
+    def reset():
+        if not Stretch._PRIVATE_PTR is None:
+            _bindings.stretch_free(Stretch._PRIVATE_PTR)
+            Stretch._PRIVATE_PTR = None
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
