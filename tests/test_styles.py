a9c4e17e82c55a7f3d9e35c3407ae0c92be0876c
Kenneth Trelborg Vestergaard <6765646+mortencombat@users.noreply.github.com>
2024-07-26 14:05:30 +0200

    Add test for GridPlacement CSS parser

--- tests/test_styles.py
+++ tests/test_styles.py
@@ -1,6 +1,7 @@
 import tinycss2
 
 from stretchable.style.parser import lstrip, rstrip, strip
+from stretchable.style.props import GridIndex, GridPlacement
 
 
 def test_strip():
@@ -38,3 +39,15 @@ def test_lrstrip():
     actual = tinycss2.serialize(rstrip(nodes))
     expected = source.rstrip()
     assert actual == expected
+
+
+def test_gridplacement():
+    css = " top: 10px; grid-column: 3 / 2; left: 20px; "
+    actual = GridPlacement.from_inline(css, axis="column")
+    assert actual.start == GridIndex.from_index(3)
+    assert actual.end == GridIndex.from_index(2)
+
+    css = " top: 10px; grid-row: 1 / span 4; left: 20px; "
+    actual = GridPlacement.from_inline(css, axis="row")
+    assert actual.start == GridIndex.from_index(1)
+    assert actual.end == GridIndex.from_span(4)
