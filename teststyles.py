fbb52879f3a805be2c3e5c5c69572283820bd585
Kenneth Trelborg Vestergaard <6765646+mortencombat@users.noreply.github.com>
2024-11-02 17:07:18 +0100

    feat: update test script

--- teststyles.py
+++ teststyles.py
@@ -1,4 +1,9 @@
 from stretchable.style import Style
+from stretchable.style.parser import adapters, simple_dimension_converter
+
+adapters.dimension_converter = lambda prop, token, context: simple_dimension_converter(
+    prop, token, context, unit_scales={"px": 1, "pt": 17.5}, require_unit=True
+)
 
 str1 = "width: 50%; aspect-ratio: 1.5"
 style1 = Style.from_str(str1)
@@ -6,7 +11,7 @@ style1 = Style.from_str(str1)
 str2 = dict(width="50%", aspect_ratio=1.5)
 style2 = Style.from_dict(str2)
 
-str3 = "margin: 10px; margin-left: 5px; margin-right: 5px; left: 25px; align-content: center; overflow: hidden; overflow-x: scroll"
+str3 = "margin: 10px; margin-left: 5pt; margin-right: 5pt; left: 25px; align-content: center; overflow: hidden; overflow-x: scroll"
 style3 = Style.from_str(str3)
 print(style3.margin)
 print(style3.align_content)
